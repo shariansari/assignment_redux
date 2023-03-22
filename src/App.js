382f1e34f73d0cde9f9ca076e2737bc72ac4255a
MALIK-PC <ansari.shariq.badshah@gmail.com>
2023-03-23 00:11:33 +0530

    done with properties

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useDispatch,useSelector } from 'react-redux';
 import  { fetchTodos } from './redux/slice/todo';
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -10,14 +10,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import { Row } from 'react-bootstrap';
 import Col from "react-bootstrap/Col";
+import { NavDropdown } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
 
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [mo, setMo] = useState([]);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (r) =>{
+    setMo(r);
+        setShow(true);
+  } 
 
   const [query,setQuery]=useState("Nature")
-  fetchTodos(query)
-
+  
   const dispatch =useDispatch();
+  useEffect(()=>{
+dispatch(fetchTodos(query))
+  },[])
   const state =useSelector(state => state);
   console.log(state,"goodjob")
   if(state.todo.isLoading){
@@ -28,8 +40,21 @@ function App() {
   return (
     <div className="App">
      <Navbar bg="primary" expand="lg">
+   
       <Container fluid style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-        
+      <NavDropdown title="Grid View" id="navbarScrollingDropdown" style={{marginRight:"10px"}}>
+              <NavDropdown.Item href="#action3">5 in a row</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                4 in a row
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                3 in a row
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                2 in a row
+              </NavDropdown.Item>
+              
+            </NavDropdown>
        
           <Form className="d-flex">
             <Form.Control
@@ -64,7 +89,7 @@ function App() {
                       {item.title}
                     </Card.Title>
                     <Card.Text style={{ fontWeight: "700" }}>
-                      {item.text}
+                      <button className='btn btn-primary' variant="primary" onClick={()=>handleShow(item)}>view Properties</button>
                     </Card.Text>
                   </Card.Body>
                 </Card>
@@ -73,6 +98,27 @@ function App() {
           </Row>
         </div>
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{mo.alt}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p> <strong>Photographer Name :</strong>{mo.photographer}</p>
+            <p><strong>Photographer Url :</strong>{mo.photographer_url}</p>
+            <p><strong>Photographer id :</strong>{mo.id}</p>
+            <p><strong>Original Height :</strong>{mo.height} px</p>
+            <p><strong>Original width :</strong>{mo.width} px</p>
+
+
+
+            </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+         
+        </Modal.Footer>
+      </Modal>
     </Container>
 
   
